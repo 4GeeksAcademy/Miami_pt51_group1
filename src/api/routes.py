7e21a5b78f3e9b6ece442c92f892a4b1083c7dbb
GISOjinga <103523730+GISOjinga@users.noreply.github.com>
2023-07-17 23:49:25 +0000

    Installed Flask_JWT_Extended

--- src/api/routes.py
+++ src/api/routes.py
@@ -28,6 +28,16 @@ def handle_login():
 
 
 
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    
+    return jsonify({"id": user.id, "username": user.username }), 200
+
+
 @api.route("/token", methods=["POST"])
 def create_token():
     username = request.json.get("username", None)

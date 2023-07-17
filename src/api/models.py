199a3d4dcd9702238aef7bc166429718fb79143d
GISOjinga <103523730+GISOjinga@users.noreply.github.com>
2023-07-17 23:13:19 +0000

    Added Create Token

--- src/api/models.py
+++ src/api/models.py
@@ -13,7 +13,6 @@ class User(db.Model):
     country = db.Column(db.String(120), unique=True, nullable=False)
     territory_state = db.Column(db.String(80), unique=False, nullable=False)
     dob = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
